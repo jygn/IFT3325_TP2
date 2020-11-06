728be19be153608913306e39ec36d518395e2b7b
jessy <jessy-g92@hotmail.com>
2020-11-05 20:29:13 -0500

    trame todo

---
+++ src/main/java/Trame.java
@@ -0,0 +1,15 @@
+public class Trame {
+
+    private byte flag;
+    private byte type;
+    private byte num;
+    private byte[] data;
+    private short CRC;
+
+//    class trame() {
+//
+//    }
+
+
+
+}
