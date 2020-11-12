e9632db026af2c0233c144eb28b9aaf3723d55db
bendag <benjamin.d@live.ca>
2020-11-12 13:54:01 -0500

    firs commit, example of socket in Java

--- src/main/java/Receiver.java
+++ src/main/java/Receiver.java
@@ -1,2 +1,45 @@
+import java.awt.color.ICC_Profile;
+import java.net.*;
+import java.io.*;
+
 public class Receiver {
+
+    private Socket socket = null;
+    private ServerSocket server = null;
+    private DataInputStream in = null;
+
+    public Receiver(int port){
+
+        try{
+            server = new ServerSocket(port);
+            System.out.println("Server started");
+
+            socket = server.accept();
+            System.out.println("Sender accepted");
+
+            in = new DataInputStream(
+                    new BufferedInputStream(socket.getInputStream()));
+
+            String line = "";
+
+            while(!line.equals("over")){
+                try{
+                    line = in.readUTF();
+                    System.out.println(line);
+                } catch (IOException i){
+                    System.out.println(i);
+                }
+            }
+            System.out.println("Closing connection");
+
+            socket.close();
+            in.close();
+        } catch (IOException i){
+            System.out.println(i);
+        }
+    }
+
+    public static void main(String args[]){
+        Receiver receiver = new Receiver(Integer.parseInt(args[0]));
+    }
 }
--- src/main/java/Sender.java
+++ src/main/java/Sender.java
@@ -1,2 +1,55 @@
+import java.net.*;
+import java.io.*;
+
 public class Sender {
+
+    private Socket socket = null;
+    private DataInputStream input = null;
+    private DataOutputStream out = null;
+
+    public Sender(String address, int port){
+        try {
+            socket = new Socket(address, port);
+            System.out.println("Connected");
+
+            input = new DataInputStream(System.in);
+
+            out = new DataOutputStream(socket.getOutputStream());
+        } catch (UnknownHostException u){
+            System.out.println(u);
+        } catch (IOException i){
+            System.out.println(i);
+        }
+
+        // string to read message from input
+        String line = "";
+
+        // keep reading until "Over" is input
+        while (!line.equals("over"))
+        {
+            try
+            {
+                line = input.readUTF();
+                out.writeUTF(line);
+            }
+            catch(IOException i)
+            {
+                System.out.println(i);
+            }
+        }
+
+        try
+        {
+            out.close();
+            socket.close();
+        }
+        catch(IOException i)
+        {
+            System.out.println(i);
+        }
+    }
+
+    public static void main(String args[]){
+        Sender sender = new Sender(args[0], Integer.parseInt(args[1]));
+    }
 }
